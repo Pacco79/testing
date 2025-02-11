4bed9ed01e19d9959aaab5a8f7608f778b3f28ff
Pacco79 <sascha.pacco@gmail.com>
2025-02-11 16:44:12 +0000

    Update fetch_events.py

--- fetch_events.py
+++ fetch_events.py
@@ -2,21 +2,26 @@ import requests
 import pandas as pd
 from flask import Flask, render_template
 
-# Fetch data from the API
-response = requests.get('http://webserver-service-events.infra/events')
-data = response.json()
-
-# Convert data to a pandas DataFrame
-df = pd.DataFrame(data)
-
-# Create the Flask application
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Convert the DataFrame to HTML1
-    html_table = df.to_html(classes='table table-striped', index=False)
-    return render_template('index.html', table=html_table)
+    try:
+        # Update the API endpoint if needed
+        response = requests.get('http://webserver-service-events.infra.svc.cluster.local/events')
+        response.raise_for_status()
+        data = response.json()
+
+        # Convert data to a pandas DataFrame
+        df = pd.DataFrame(data)
+
+        # Convert the DataFrame to HTML
+        html_table = df.to_html(classes='table table-striped', index=False)
+        return render_template('index.html', table=html_table)
+
+    except requests.exceptions.RequestException as e:
+        return f"Error fetching data: {e}", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
